1b84ad1913a6c06187b04e90b6a8de5be6df387d
Constantin <constantin.huidu@gmail.com>
2024-01-27 14:17:59 +0200

    mock data

---
+++ src/mockData/books.ts
@@ -0,0 +1,52 @@
+const books = [
+  {
+    id: 1,
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    genre: "Fiction, Southern Gothic, Coming-of-Age",
+    description:
+      "Set in the racially charged American South during the 1930s, 'To Kill a Mockingbird' follows the story of Scout Finch, a young girl, and her brother Jem, as their father, lawyer Atticus Finch, defends a black man accused of raping a white woman. The novel explores themes of racial injustice, moral growth, and compassion.",
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "George Orwell",
+    genre: "Dystopian Fiction",
+    description:
+      "In a totalitarian future society, '1984' depicts the life of Winston Smith, a low-ranking member of the ruling Party. The novel explores the consequences of unchecked government power, surveillance, and the manipulation of truth. Orwell's classic serves as a cautionary tale about the dangers of authoritarianism.",
+  },
+  {
+    id: 3,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    genre: "Fiction, Jazz Age, Tragedy",
+    description:
+      "Set against the backdrop of the Roaring Twenties, 'The Great Gatsby' tells the story of Jay Gatsby's extravagant pursuit of wealth and love, as narrated by Nick Carraway. This American classic explores themes of decadence, social class, and the elusive nature of the American Dream.",
+  },
+  {
+    id: 4,
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel García Márquez",
+    genre: "Magical Realism, Literary Fiction",
+    description:
+      "This Nobel Prize-winning novel chronicles the Buendía family over several generations in the fictional town of Macondo. Blending magical elements with historical events, Márquez weaves a rich tapestry of love, power, and the cycle of human existence, creating a captivating and complex narrative.",
+  },
+  {
+    id: 5,
+    title: "The Hitchhiker's Guide to the Galaxy",
+    author: "Douglas Adams",
+    genre: "Science Fiction, Comedy",
+    description:
+      "A humorous and absurd journey through space, 'The Hitchhiker's Guide to the Galaxy' follows Arthur Dent, an unwitting human survivor of Earth's destruction, as he travels through the cosmos with Ford Prefect, Zaphod Beeblebrox, and other eccentric characters. Adams' wit and satire make this a delightful and entertaining science fiction classic.",
+  },
+  {
+    id: 6,
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    genre: "High Fantasy",
+    description:
+      "Comprising three volumes – 'The Fellowship of the Ring,' 'The Two Towers,' and 'The Return of the King' – 'The Lord of the Rings' is an epic high fantasy saga set in the fictional world of Middle-earth. The story follows Frodo Baggins, a hobbit tasked with destroying a powerful ring that could bring about the downfall of the world. Alongside a diverse fellowship, including Aragorn, Legolas, Gimli, and Gandalf, Frodo embarks on a perilous journey, facing dark forces and overcoming challenges. Tolkien's work is celebrated for its intricate world-building, rich mythology, and exploration of themes such as friendship, heroism, and the battle between good and evil.",
+  },
+];
+
+console.log(books);
